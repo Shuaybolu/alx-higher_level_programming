08972052eb3eae1828f0af34be1bffe878a4bdae
“Shuaybolu <olusesishuaib@yahoo.com>
2024-10-09 15:08:20 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
+"""Module that defines a Rectangle class"""
+
 
 class Rectangle:
     """Class that defines a rectangle"""
-    
+
     def __init__(self, width=0, height=0):
         """Initialize rectangle with optional width and height
-        
+
         Args:
             width (int): width of rectangle
             height (int): height of rectangle
         """
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         """Get the width of rectangle"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         """Set the width of rectangle
-        
+
         Args:
             value (int): width value to set
-            
+
         Raises:
             TypeError: if width is not an integer
             ValueError: if width is less than 0
@@ -34,19 +36,19 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """Get the height of rectangle"""
         return self.__height
-        
-    @height.setter 
+
+    @height.setter
     def height(self, value):
         """Set the height of rectangle
-        
+
         Args:
             value (int): height value to set
-            
+
         Raises:
             TypeError: if height is not an integer
             ValueError: if height is less than 0
